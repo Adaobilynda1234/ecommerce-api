64a626dec821fb8415f2dd9b37937cc0409d5393
Adaobi Okwuosa <adaobilynda1234@gmail.com>
2025-07-22 23:08:40 +0100

    migrated to mongodb atlas

--- models/Product.js
+++ models/Product.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
 // productschema
-const productSchema = new mongoose.Schema({
-  productName: { type: String, required: true },
-  ownerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    productName: { type: String, required: true },
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    cost: { type: Number, required: true },
+    productImages: [{ type: String }],
+    description: { type: String, required: true },
+    stockStatus: { type: String, required: true },
   },
-  cost: { type: Number, required: true },
-  productImages: [{ type: String }],
-  description: { type: String, required: true },
-  stockStatus: { type: String, required: true },
-});
+  {
+    timestamps: true, // Adds createdAt and updatedAt
+  }
+);
 
 module.exports = mongoose.model("Product", productSchema);
--- models/user.js
+++ models/user.js
@@ -2,12 +2,17 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 // userschema
-const userSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "customer"], default: "customer" },
-});
+const userSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "customer"], default: "customer" },
+  },
+  {
+    timestamps: true, // Adds createdAt and updatedAt
+  }
+);
 
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
